0a29cb6d2c51bf871793bbd78f02d7c278ccafc8
thomasmore <mcbolsh@gmail.com>
2021-10-18 18:20:31 +0300

    Update init.lua

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -72,8 +72,8 @@ cmp.setup({
       select = true,
     }),
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        feedkey("<C-n>", "n")
+      if cmp.visible() then
+        cmp.select_next_item()
       elseif vim.fn["vsnip#available"]() == 1 then
         feedkey("<Plug>(vsnip-expand-or-jump)", "")
       elseif has_words_before() then
@@ -84,8 +84,8 @@ cmp.setup({
     end, { "i", "s" }),
 
     ["<S-Tab>"] = cmp.mapping(function()
-      if vim.fn.pumvisible() == 1 then
-        feedkey("<C-p>", "n")
+      if cmp.visible() then
+        cmp.select_prev_item()
       elseif vim.fn["vsnip#jumpable"](-1) == 1 then
         feedkey("<Plug>(vsnip-jump-prev)", "")
       end
