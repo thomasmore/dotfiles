947f4de483c9f7a4139490f99035035022c6c63d
thomasmore <mcbolsh@gmail.com>
2022-04-12 21:39:31 +0300

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -87,7 +87,7 @@ return require('packer').startup(function()
   }
   use { 'alvarosevilla95/luatab.nvim', config = function() require('luatab').setup() end }
 
-  use { 'tpope/vim-fugitive', cmd = { 'G', 'Git', 'Gedit', 'Gread', 'Gwrite', 'Gdiff', 'Gclog', 'Gdiffsplit!' } }
+  use { 'tpope/vim-fugitive', cmd = { 'G', 'Git', 'Gedit', 'Gread', 'Gwrite', 'Gdiff', 'Gclog', 'Gdiffsplit!' } }  -- TODO enable diff mode
   use { 'tanvirtin/vgit.nvim', config = function() require('vgit').setup() end }
   use { 'sindrets/diffview.nvim', config = function() require('diffview').setup() end }
 
