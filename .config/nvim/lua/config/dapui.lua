20c1be05a14a73b9ed199d17ad4b095920610e93
thomasmore <mcbolsh@gmail.com>
2022-06-29 15:54:21 +0300

    Update dapui.lua

--- .config/nvim/lua/config/dapui.lua
+++ .config/nvim/lua/config/dapui.lua
@@ -1,6 +1,12 @@
 local dapui = require('dapui')
 local dap = require('dap')
-dapui.setup()
+dapui.setup {
+  mappings = {
+    -- Use a table to apply multiple mappings
+    expand = { "<cr>", "<RightMouse>" },
+    open = { "o", "<2-LeftMouse>" },
+  },
+}
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
 end
