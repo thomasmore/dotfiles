8a1089a7f7a023f5eb4323e96b58ca8d89895fd2
Bolshov Maxim <bolshov.maxim@huawei.com>
2022-09-16 14:51:10 +0300

    Explicitly state tokyonight style

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -240,6 +240,7 @@ return require('packer').startup(function()
     'folke/tokyonight.nvim',
     config = function()
       require('tokyonight').setup {
+        style = 'storm',
         styles = {
           comments = 'NONE',
           keywords = 'NONE',
