a81ed97d24f31f296c2d13d99e55fdf86bc8376d
thomasmore <mcbolsh@gmail.com>
2022-10-04 20:19:39 +0300

    Update dapui.lua

--- .config/nvim/lua/config/dapui.lua
+++ .config/nvim/lua/config/dapui.lua
@@ -8,16 +8,36 @@ dapui.setup {
     expand = { '<cr>', '<RightMouse>' },
     open = { 'o', '<2-LeftMouse>' },
   },
+  layouts = {
+    {
+      elements = {
+        { id = 'scopes', size = 0.25 },
+        'breakpoints',
+        'stacks',
+        'watches',
+      },
+      size = 40,
+      position = 'left',
+    },
+    {
+      elements = {
+        'repl',
+      },
+      size = 0.25,
+      position = 'bottom',
+    },
+  },
 }
+
 dap.listeners.after.event_initialized['dapui_config'] = function()
   dapui.open()
-end
-dap.listeners.before.event_terminated['dapui_config'] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited['dapui_config'] = function()
-  dapui.close()
 end
 
 nmap(',e', dapui.eval, 'Eval word under cursor')
 vmap(',e', dapui.eval, 'Eval in debug')
+nmap(',q', function()
+  if dap.session() then
+    dap.terminate()
+  end
+  dapui.close()
+end, 'Close debug UI')
