e1f0d0b03dea4fa2ab4447b9b8a3a3201c1df01c
Bolshov Maxim <bolshov.maxim@huawei.com>
2022-10-21 18:59:39 +0300

    update telescope config

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,22 +1,46 @@
--- TODO vertical layout_strategy for lsp_definitions, lsp_references, lsp_implementations, live_grep
--- TODO add mapping for lsp_dynamic_workspace_symbol
--- TODO find_files and live_grep in .gitignore files also
-require('telescope').setup{
-  -- defaults = {
-  --   layout_config = {
-  --     vertical = { width = 0.85 }
-  --   },
-  --   layout_strategy = 'vertical',
-  -- }
-}
-require('telescope').load_extension('zf-native')
+local telescope = require('telescope')
+local builtins = require('telescope.builtin')
+local themes = require('telescope.themes')
+local config = require('telescope.config')
 
 local nmap = require('utils').nmap
-local telescope = require('telescope.builtin')
-local themes = require('telescope.themes')
+
+
+local vimgrep_arguments = { unpack(config.values.vimgrep_arguments) }
+table.insert(vimgrep_arguments, '--no-ignore')
+
+telescope.setup{
+  defaults = {
+    layout_config = {
+      vertical = { width = 0.9 },
+      horizontal = { width = 0.9, preview_width = 0.5 },
+    },
+    layout_strategy = 'vertical',
+  },
+  pickers = {
+    find_files = {
+      layout_strategy = 'horizontal',
+      no_ignore = true,
+      no_ignore_parent = true
+    },
+    oldfiles = {
+      layout_strategy = 'horizontal',
+    },
+    buffers = {
+      layout_strategy = 'horizontal',
+    },
+    live_grep = {
+      vimgrep_arguments = vimgrep_arguments,
+    },
+    grep_string = {
+      vimgrep_arguments = vimgrep_arguments,
+    }
+  }
+}
+telescope.load_extension('zf-native')
 
 local function spell_suggest()
-  telescope.spell_suggest(
+  builtins.spell_suggest(
     themes.get_cursor {
       prompt_title = "",
       layout_config = {
@@ -27,15 +51,15 @@ local function spell_suggest()
   )
 end
 
-nmap('<c-p>', telescope.find_files, 'Find file')
-nmap('<c-l>', telescope.current_buffer_fuzzy_find, 'Live search in current file')
-nmap('<c-h>', telescope.oldfiles, 'Open recent file')
-nmap('<leader>gg', telescope.grep_string, 'Grep for word under the cursor')
-nmap('<leader>lg', telescope.live_grep, 'Live grep for typed string')
-nmap('<leader>b', telescope.buffers, 'List open buffers')
-nmap('gco', telescope.git_branches, 'List git branches')
-nmap('gr', telescope.lsp_references, 'List references')
-nmap('gd', telescope.lsp_definitions, 'Go to definition')
-nmap('gi', telescope.lsp_implementations, 'List implementations')
-nmap('c-RightMouse', '<LeftMouse><cmd>Telescope lsp_definitions<cr>', 'Go to definition or list them')
+nmap('<c-p>', builtins.find_files, 'Find file')
+nmap('<c-l>', builtins.current_buffer_fuzzy_find, 'Live search in current file')
+nmap('<c-h>', builtins.oldfiles, 'Open recent file')
+nmap('<leader>gg', builtins.grep_string, 'Grep for word under the cursor')
+nmap('<leader>gw', builtins.lsp_dynamic_workspace_symbols, 'Live Grep for workspace symbol')
+nmap('<leader>gl', builtins.live_grep, 'Live grep for typed string')
+nmap('<leader>b', builtins.buffers, 'List open buffers')
+nmap('gco', builtins.git_branches, 'List git branches')
+nmap('gr', builtins.lsp_references, 'List references')
+nmap('gd', builtins.lsp_definitions, 'Go to definition')
+nmap('gi', builtins.lsp_implementations, 'List implementations')
 nmap('<leader>sp', spell_suggest, 'Spell suggestions')
