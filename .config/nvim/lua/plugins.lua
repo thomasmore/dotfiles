67520557b74f0fce8fc341f21e97a7a861be31e4
thomasmore <mcbolsh@gmail.com>
2022-10-25 15:10:52 +0300

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -386,6 +386,9 @@ return require('packer').startup(function()
   use {
     'RRethy/vim-illuminate',
     config = function()
+      require('illuminate').configure {
+        modes_denylist = {'i'},
+      }
       vim.api.nvim_set_hl(0, 'IlluminatedWordText', { link = 'Visual' })
       vim.api.nvim_set_hl(0, 'IlluminatedWordRead', { link = 'Visual' })
       vim.api.nvim_set_hl(0, 'IlluminatedWordWrite', { link = 'Visual' })
