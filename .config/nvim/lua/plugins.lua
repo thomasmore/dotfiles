53802b785b2ca7b628a2bbf7fa5bb501737b38a5
Maxim Bolshov <bolshov.maxim@huawei.com>
2023-01-24 15:08:23 +0300

    update nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -561,4 +561,13 @@ require 'paq' {
       }
     end
   };
+
+  {
+    'chomosuke/term-edit.nvim',
+    config = function()
+      require('term-edit').setup {
+        prompt_end = '[🔥💩] '
+      }
+    end
+  };
 }
