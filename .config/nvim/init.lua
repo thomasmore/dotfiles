358839f19b1af31c083b963658a7e214fb6760f4
thomasmore <mcbolsh@gmail.com>
2023-02-13 18:18:27 +0300

    Add TODOs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- TODO:
+-- * disable autoindent for norg files
+-- * <leader>wt doesn't work as intended
+-- * less boilerplate in autcmd: pass pattern and callback as arguments
 pcall(require, 'impatient')
 require 'settings'
 require 'plugins'
