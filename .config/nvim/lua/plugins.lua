9ac0b5a479af000de4a133f232c5ba935db7479e
thomasmore <mcbolsh@gmail.com>
2023-03-16 12:26:54 +0300

    Update plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -153,7 +153,7 @@ require 'paq' {
       local nmap = require('utils').nmap
       local vmap = require('utils').vmap
       nmap('<leader>gh', function() vim.cmd.DiffviewFileHistory('%') end, 'View Git history for current file')
-      vmap('<leader>gh', vim.cmd.DiffviewFileHistory, 'View Git history for selected range')
+      vmap('<leader>gh', ':DiffviewFileHistory<cr>', 'View Git history for selected range')
       nmap('<leader>go', function() vim.cmd.DiffviewOpen('HEAD^') end, 'View diff for last commit')
       nmap('<leader>gc', vim.cmd.DiffviewClose, 'Close Diffview tab')
     end,
