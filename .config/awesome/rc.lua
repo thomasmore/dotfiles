06f85778a55c7dc2ecb6b141cdbcc1944428daf8
Thomas More <thomasmore@github.com>
2024-06-24 14:02:14 +0300

    update awesome config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -51,7 +51,7 @@ end
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 beautiful.useless_gap = 5
-beautiful.wallpaper = "$HOME/Pictures/wallpaper.png"
+beautiful.wallpaper = os.getenv("HOME").."/Pictures/wallpaper.png"
 
 -- This is used later as the default terminal and editor to run.
 terminal = "wezterm"
