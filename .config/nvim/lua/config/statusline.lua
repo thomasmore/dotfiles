867e6ac25c3f37d5c6f9a5b8b0c05cea080036da
thomasmore <mcbolsh@gmail.com>
2024-10-23 15:58:13 +0300

    Update statusline.lua

--- .config/nvim/lua/config/statusline.lua
+++ .config/nvim/lua/config/statusline.lua
@@ -4,7 +4,7 @@ local cmake = require('cmake')
 
 local get_lsp_client = function ()
   local buf_ft = vim.api.nvim_get_option_value('filetype', { scope = 'local' })
-  local clients = vim.lsp.get_active_clients()
+  local clients = vim.lsp.get_clients()
   if next(clients) == nil then
     return ''
   end
