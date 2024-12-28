6182941e87ac372de9a4e6bbe07aa872ef6bbcfa
Thomas More <thomasmore@github.com>
2024-12-28 14:16:27 +0300

    update kitty and nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -481,34 +481,6 @@ require 'paq' {
     end
   },
 
-  {
-    'm4xshen/hardtime.nvim',
-    config = function()
-      require('hardtime').setup {
-        disable_mouse = false,
-        notification = false,
-        disabled_keys = {
-          ['<Up>'] = {},
-          ['<Down>'] = {},
-          ['<Left>'] = {},
-          ['<Right>'] = {},
-        },
-        restricted_keys = {
-          ['h'] = { 'n', 'x' },
-          ['j'] = { 'n', 'x' },
-          ['k'] = { 'n', 'x' },
-          ['l'] = { 'n', 'x' },
-          ['-'] = { 'n', 'x' },
-          ['+'] = { 'n', 'x' },
-          ['gj'] = { 'n', 'x' },
-          ['gk'] = { 'n', 'x' },
-          ['<CR>'] = { 'n', 'x' },
-          ['<C-M>'] = { 'n', 'x' },
-          ['<C-N>'] = { 'n', 'x' },
-        },
-      }
-    end
-  },
 
   {
     'MeanderingProgrammer/render-markdown.nvim',
